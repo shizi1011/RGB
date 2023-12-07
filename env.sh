bce58b1d67e2f08a1e1b0f7396e10393ec83be23
shizi1011 <88260674+shizi1011@users.noreply.github.com>
2023-12-07 17:49:04 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
-conda create --name evalue python=3.10.0
+conda create --name evalue python
 conda activate evalue
 pip install torch==2.0.0+cu118 --index-url https://download.pytorch.org/whl/cu118
-pip install transformers==4.31.0
+pip install git+https://github.com/huggingface/transformers.git
 pip install wandb
 pip install accelerate
 pip install sentencepiece
@@ -10,3 +10,4 @@ pip install tiktoken
 pip install einops
 pip install fschat
 pip install nvgpu
+pip install bitsandbytes
