abe1a76d3706536929623f01239e15ec0fb9857e
shizi1011 <88260674+shizi1011@users.noreply.github.com>
2023-12-15 10:57:26 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 conda create --name evalue python
 conda activate evalue
-pip install torch==2.0.0+cu118 --index-url https://download.pytorch.org/whl/cu118
+# pip install torch==2.0.0+cu118 --index-url https://download.pytorch.org/whl/cu118
+pip install torch --index-url https://download.pytorch.org/whl/cu118
 pip install git+https://github.com/huggingface/transformers.git
 pip install wandb
 pip install accelerate
